1784e43f89fe0c87cc8cdd5389e7a79f285a974b
greeness <yang@scopely.com>
2013-01-18 15:39:13 -0800

    fix merge to extend lookup

--- merge.py
+++ merge.py
@@ -8,21 +8,19 @@ import json
 N = 5*5
 def load_anagrams():
     anagrams = {}
-    #anagrams_lookup = defaultdict(list)
+    anagrams_lookup = defaultdict(set)
     bags = []
     with open('resource/anadict.txt', 'r') as file_handle:
         for line in file_handle:
             words = line.split()
             if len(words[0]) > N: continue
             anagrams[words[0]] = len(words[1:])
-            #anagrams_lookup[words[0]] = words[1:]
+            for w in words[1:]:
+                anagrams_lookup[words[0]].add(w)
             bags.append(Counter(words[0]))
-            #print '-'*100
-            #print anagrams_lookup[words[0]]
-            #print anagrams[words[0]]
-    return bags, anagrams
+    return bags, anagrams_lookup
 
-def merge(bags, anagrams):
+def merge(bags, anagrams_lookup):
     mcount = 0
     sorted_bags = sorted(bags, key=lambda b: sum(b.values()))
     for b1 in sorted_bags:
@@ -30,27 +28,28 @@ def merge(bags, anagrams):
         for b2 in sorted_bags:
             # skip itself
             if b1 == b2: continue
-            # deepcopy required otherwise bags get modified
-            b = deepcopy(b2)
             # get a diff
-            b.subtract(b1)
+            b = b2 - b1
             if all([v>=0 for v in b.values()]):
                 longer_key = ''.join(sorted(b2.elements()))
-                # merging the count              
-                anagrams[longer_key] += anagrams[shorter_key]
+                # merging the count
+                print 'before long' , anagrams_lookup[longer_key]
+                print 'before short', anagrams_lookup[shorter_key]             
+                anagrams_lookup[longer_key] = anagrams_lookup[longer_key] | anagrams_lookup[shorter_key]
+                print 'after', anagrams_lookup[longer_key]
                 
-        del anagrams[shorter_key]
+        del anagrams_lookup[shorter_key]
         sorted_bags.remove(b1)
         mcount += 1        
         print "shorter key %26s, unprocessed anagrams %6d, %.3f done" % \
             (shorter_key, len(sorted_bags), mcount*100./len(bags))
                 
-    json.dump(anagrams, open('resource/merged_anagrams.json', 'w+'), indent=2)    
+    json.dump(anagrams_lookup, open('resource/merged_anagrams_lookup.json', 'w+'), indent=2)    
     print 'done'   
                 
         
 if __name__ == '__main__':
-    bags, anagrams = load_anagrams()
+    bags, anagrams_lookup = load_anagrams()
     #bb = sorted(bags, key=lambda b: sum(b.values()))[:50]
     #pprint(bb)
-    merge(bags, anagrams)
+    merge(bags, anagrams_lookup)
