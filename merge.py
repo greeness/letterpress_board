bfe052acfc969005b7da61b32d6024db4beaae58
greeness <yang@scopely.com>
2013-01-17 18:10:44 -0800

    fix bug in merge algorithm, keep only count instead of list of sub-anagrams in this version

--- merge.py
+++ merge.py
@@ -7,34 +7,42 @@ import json
 
 N = 5*5
 def load_anagrams():
-    anagrams = defaultdict(list)
+    anagrams = {}
+    #anagrams_lookup = defaultdict(list)
     bags = []
     with open('resource/anadict.txt', 'r') as file_handle:
         for line in file_handle:
             words = line.split()
             if len(words[0]) > N: continue
-            anagrams[words[0]] = words[1:]
+            anagrams[words[0]] = len(words[1:])
+            #anagrams_lookup[words[0]] = words[1:]
             bags.append(Counter(words[0]))
-    
+            #print '-'*100
+            #print anagrams_lookup[words[0]]
+            #print anagrams[words[0]]
     return bags, anagrams
 
 def merge(bags, anagrams):
     mcount = 0
-    for b1 in bags:
-        longer_key= ''.join(sorted(b1.elements()))
-        print longer_key
-        for b2 in bags:
+    sorted_bags = sorted(bags, key=lambda b: sum(b.values()))
+    for b1 in sorted_bags:
+        shorter_key= ''.join(sorted(b1.elements()))
+        for b2 in sorted_bags:
+            # skip itself
             if b1 == b2: continue
-            b = deepcopy(b1)
-            b.subtract(b2)
+            # deepcopy required otherwise bags get modified
+            b = deepcopy(b2)
+            # get a diff
+            b.subtract(b1)
             if all([v>=0 for v in b.values()]):
-                bags.remove(b2)
-                key = ''.join(sorted(b2.elements()))
+                longer_key = ''.join(sorted(b2.elements()))
+                # merging the count              
+                anagrams[longer_key] += anagrams[shorter_key]
                 
-                anagrams[longer_key].extend(anagrams[key])
-                del anagrams[key]
-                mcount += 1
-                print longer_key, len(bags), mcount, len(anagrams[longer_key]), "%.3f percent done" % (mcount*100./(mcount +len(bags)))
+        del anagrams[shorter_key]
+        mcount += 1        
+        print "shorter key %26s, unprocessed anagrams %6d, %.3f done" % \
+            (shorter_key, len(anagrams), mcount*100./len(bags))
                 
     json.dump(anagrams, open('resource/merged_anagrams.json', 'w+'), indent=2)    
     print 'done'   
@@ -42,4 +50,6 @@ def merge(bags, anagrams):
         
 if __name__ == '__main__':
     bags, anagrams = load_anagrams()
+    #bb = sorted(bags, key=lambda b: sum(b.values()))[:50]
+    #pprint(bb)
     merge(bags, anagrams)
